4ecb99d9a07e41c3c378b0603cb43cf047dea552
Norticus36 <32337930+Norticus36@users.noreply.github.com>
2017-10-14 18:05:10 +0200

    renamed to color_checker

--- color_sensor/try.cc
+++ color_sensor/try.cc
@@ -1,4 +1,4 @@
-#include "color_sensor.h"
+#include "color_checker.h"
 #include "stream_reader.h"
 
 
@@ -6,17 +6,17 @@
 int main(int argc, char **argv)
 {
     
-    std::vector<color_sensor::area> x = 
+    std::vector<color_checkerr::area> x = 
     {
-        color_sensor::area(0,0,1280,720,color(186,193,192),25)
+        color_checker::area(0,0,1280,720,color(186,193,192),25)
     };
-    color_sensor c(x);
+    color_checker c(x);
     //picture p("colortest2.png");
     //c.__test_color(p);
     stream_reader sr("rtsp://admin:Almafa123@192.168.1.108:554");
     while (true)
     {
-        sr.process_frame(std::bind(&color_sensor::check_for_color,&c,std::placeholders::_1));
+        sr.process_frame(std::bind(&color_checker::check_for_color,&c,std::placeholders::_1));
         for (int i=0;i<x.size();i++)
         {
             const bool b = c.get_result(i);
