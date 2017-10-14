dc51d129abba8e49a1e53812658b98870e095216
Norticus36 <32337930+Norticus36@users.noreply.github.com>
2017-10-14 11:57:11 +0200

    Add files via upload
    fixed and tested the color sensor

---
+++ color.h
@@ -0,0 +1,18 @@
+#ifndef COLOR
+#define COLOR
+#include <cmath>
+#include <cstdint>
+struct color {
+	uint8_t red, green, blue;
+	color() : red(0), green(0), blue(0) {}
+	color(uint8_t r, uint8_t g, uint8_t b) : red(r), green(g), blue(b) {}
+	bool is_near(color _c, uint8_t _eps)
+	{
+		return
+			((abs(_c.red - red) <= _eps) &&
+			(abs(_c.green - green) <= _eps) &&
+				(abs(_c.blue - blue) <= _eps));
+	}
+};
+
+#endif
---
+++ picture.h
@@ -0,0 +1,40 @@
+#ifndef PICTURE
+#define PICTURE
+#include <Imlib2.h>
+#include "color.h"
+//#include <cstdint> already included in color.h ??
+
+struct picture 
+{
+	int width;
+	int height;
+	DATA32* data;
+
+	picture(const std::string &path) {
+		Imlib_Image image;
+		image = imlib_load_image(path.c_str());
+		if (image)
+		{
+			imlib_context_set_image(image);
+			width = imlib_image_get_width();
+			height = imlib_image_get_height();
+			data = imlib_image_get_data_for_reading_only();
+		}
+		else
+		{
+			std::cerr << "Error: cannot load image!" << std::endl;
+		}
+	}
+
+	color get(int x, int y) const
+	{
+		color c;
+		uint8_t *p = reinterpret_cast<uint8_t*>(&data[y*width + x]);
+		c.red = p[2];
+		c.green = p[1];
+		c.blue = p[0];
+		return c;
+	}
+
+};
+#endif
