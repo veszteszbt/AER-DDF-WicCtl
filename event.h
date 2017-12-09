06f85a5fe23ee539f1fe5ff03cee04e99223803c
Géza Török <geza.torok@neuroforge.org>
2017-12-09 21:05:54 +0100

    Added event entity

---
+++ event.h
@@ -0,0 +1,45 @@
+#ifndef WIC_EVENT_H
+# define WIC_EVENT_H
+# include <chrono>
+# include <sched/listener.h>
+# include <wicp/local_property.h>
+namespace wic {
+
+template<typename TConfig>
+class event
+{
+		struct property_config : public property_config_base
+		{
+			typedef uint64_t cfg_value_type;
+
+			static const uint32_t cfg_class_id = TConfig::cfg_class_id;
+
+			static const uint32_t cfg_member_id = TConfig::cfg_member_id;
+
+			static const uint32_t cfg_cooldown_time = 0;
+
+			static const bool cfg_commit_change_only = true;
+		};
+		typedef wicp::local_property<property_config> property;
+public:
+
+	static void init()
+	{ property::init(); }
+
+	static void uninit()
+	{ property::uninit(); }
+
+	static void monitor()
+	{
+		if(TConfig::cfg_condition())
+		{
+			property::value(std::chrono::time_point_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now()).time_since_epoch().count());
+			TConfig::cfg_action();
+		}
+	}
+
+	constexpr static sched::listener &on_raised = property::on_change;
+};
+
+}
+#endif
