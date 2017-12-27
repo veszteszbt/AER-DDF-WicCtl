1bee6c5737396d0e92aa89986d04a0ac6ccab678
Török Géza <torokg87@gmail.com>
2017-12-27 12:04:41 +0100

    Applied types::time::timestamp function to generate timestamps

--- journal.cc
+++ journal.cc
@@ -1,4 +1,5 @@
 #include <journal.h>
+#include <types/time.h>
 #include <cctype>
 #include <iomanip>
 #include <sstream>
@@ -32,13 +33,7 @@ std::ostream &journal::get_stream(const std::string&)
 }
 
 std::string journal::get_timestamp()
-{
-	time_t now;
-	time(&now);
-	char buf[64];
-	strftime(buf, sizeof buf, "%Y.%m.%d %H:%M:%S", gmtime(&now));
-	return buf;
-}
+{ return ::types::time::timestamp(std::chrono::high_resolution_clock::now()); }
 
 std::vector<std::string> journal::get_domains()
 {
