452ea716228746c40f573d7bc6c2b057df09c34b
Norticus36 <todi97@gmail.com>
2018-04-14 13:55:00 +0200

    Syntactical parser - Drop 1

--- test/earpc/pair_test_a.cc
+++
@@ -1,5 +0,0 @@
-#include <earpc/earpc.h>
-#include <iostream>
-static const uint16_t sport = 1234;
-static const uint16_t dport = 1235;
-#include "pair_test.h"
--- test/wicp/pair_test_b.cc
+++
@@ -1,5 +0,0 @@
-#include <earpc/earpc.h>
-#include <iostream>
-static const uint16_t sport = 1235;
-static const uint16_t dport = 1234;
-#include "pair_test.h"
